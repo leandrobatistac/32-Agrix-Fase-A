5edcca129efe44e3d1ade0f10ea8bfdbb9c7c90d
leandrobatista <leandrobcrv@gmail.com>
2023-10-19 13:26:44 -0300

    Requisito 07

--- src/main/java/com/betrybe/agrix/controller/CropsController.java
+++ src/main/java/com/betrybe/agrix/controller/CropsController.java
@@ -4,10 +4,12 @@ import com.betrybe.agrix.dto.CropDto;
 import com.betrybe.agrix.model.entities.Crop;
 import com.betrybe.agrix.service.CropService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +39,18 @@ public class CropsController {
                     crop.getName(),
                     crop.getPlantedArea(),
                     crop.getFarm().getId())));
+  }
+
+  /**
+   * Javadoc.
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getCropById(@PathVariable Long id) {
+    Optional<Crop> cropById = cropService.getCropById(id);
+    if (cropById.isPresent()) {
+      return ResponseEntity.status(HttpStatus.OK).body(CropDto.convertToCrop(cropById.get()));
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Plantação não encontrada!");
+    }
   }
 }
