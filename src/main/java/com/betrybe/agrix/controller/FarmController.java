f60aa6ad43d2c29aa40aa982c3621268eee25446
leandrobatista <leandrobcrv@gmail.com>
2023-10-19 00:20:16 -0300

    Requisito 02

--- src/main/java/com/betrybe/agrix/controller/FarmController.java
+++ src/main/java/com/betrybe/agrix/controller/FarmController.java
@@ -3,9 +3,11 @@ package com.betrybe.agrix.controller;
 import com.betrybe.agrix.dto.FarmDto;
 import com.betrybe.agrix.model.entities.Farm;
 import com.betrybe.agrix.service.FarmService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,11 @@ public class FarmController {
   public ResponseEntity<Farm> insertFarm(@RequestBody FarmDto farmDto) {
     Farm newFarm = farmService.insert(farmDto.convertToFarm());
     return ResponseEntity.status(HttpStatus.CREATED).body(newFarm);
+  }
+
+  @GetMapping()
+  public ResponseEntity<Object> getAll() {
+    List<Farm> allFarms = farmService.getAll();
+    return ResponseEntity.status(HttpStatus.OK).body(allFarms);
   }
 }
--- src/main/java/com/betrybe/agrix/service/FarmService.java
+++ src/main/java/com/betrybe/agrix/service/FarmService.java
@@ -2,6 +2,7 @@ package com.betrybe.agrix.service;
 
 import com.betrybe.agrix.model.entities.Farm;
 import com.betrybe.agrix.model.repositories.FarmRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,9 @@ public class FarmService {
 
   public Farm insert(Farm farm) {
     return farmRepository.save(farm);
+  }
+
+  public List<Farm> getAll() {
+    return farmRepository.findAll();
   }
 }
