01bcdcc068ceb9088f86ddc5bacb665d082d9be7
leandrobatista <leandrobcrv@gmail.com>
2023-10-19 01:57:54 -0300

    Requisito 08

---
+++ src/main/java/com/betrybe/agrix/model/repositories/CropRepository.java
@@ -0,0 +1,13 @@
+package com.betrybe.agrix.model.repositories;
+
+import com.betrybe.agrix.model.entities.Crop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Javadoc.
+ */
+@Repository
+public interface CropRepository extends JpaRepository<Crop, Long> {
+
+}
